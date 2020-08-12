6b0142a05d116769f50a04dbcdc474f6a9327c25
leeolivier <leeolivier@LAPTOP-16KCDMPH.localdomain>
2020-08-12 10:29:58 +0200

    section 3

--- venv/app/forms.py
+++ venv/app/forms.py
@@ -1,26 +1,24 @@
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, PasswordField, SubmitField, TextField, BooleanField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Regexp
 from app.models import User
-from re import compile
-PASSWORD_REGEX = compile(r'\A(?=\S*?\d)(?=\S*?[A-Z])(?=\S*?[a-z])\S{6,}\Z')
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+    
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators = [DataRequired()])
     surname = StringField('Surname', validators = [DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators= [DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-   
+    email = StringField('Email', validators= [DataRequired(), Email()])
+    password = PasswordField('Password', validators= [DataRequired(),Length(min=6, message='between 6 and 10 Characters long!'), Regexp (r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$' , message = 'at least 1 number, 1 upper, 1 lower, 1 special character')])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password', message = 'Passwords must match.')])
     submit = SubmitField('Register')
-
-
+    
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
@@ -30,3 +28,5 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
--- venv/app/routes.py
+++ venv/app/routes.py
@@ -3,15 +3,12 @@ from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from app.forms import RegistrationForm
-from app.forms import LoginForm
-from app import db
-
+from app.forms import RegistrationForm, LoginForm, EmptyForm
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('profile'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -25,7 +22,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('profile'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -33,21 +30,62 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        
     return render_template('login.html', title='Sign In', form=form)
-
-
-
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
-
+    return redirect(url_for('profile'))
 
 @app.route('/')
-@app.route('/index')
+@app.route('/profile')
 @login_required
-def index():    
-    return render_template("index.html", title='Home_Page', posts=posts) 
+def profile():    
+    return render_template("profile.html", title='Home_Page', posts=posts)
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('profile'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('profile'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('profile'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('profile'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template('user.html', user=user, form=form)
    
